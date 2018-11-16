9a3f8fda705f34fe1f395d0e0ecc94551dc8f745
Mayllon Veras <mayllonveras@gmail.com>
2018-11-16 20:43:54 -0300

    Formatação de real para texto adicionada.

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ function calcula(){
         valor = textoParaReal(valor);
         var quantidade = $(produtos[i])
                         .find(".quantidade")[0].value;
-    $(produtos[i]).find(".subtotal").text(valor*quantidade);
-    total = total + valor * quantidade;
+        var subTotal = valor * quantidade;
+    $(produtos[i]).find(".subtotal").text(realParaTexto(subTotal));
+    total += subTotal;
     }
-    $("#total").text(total);
+    $("#total").text(realParaTexto(total));
 }
 
 function textoParaReal(texto) {
@@ -18,6 +19,29 @@ function textoParaReal(texto) {
     textoLimpo = textoLimpo.replace(",", ".");
 
     return parseFloat(textoLimpo);
+}
+
+function realParaTexto(valorReal){
+    var texto = ""+valorReal;
+    var textoVet = [texto, "00"];
+    if(texto.indexOf(".") != -1)
+        textoVet = texto.split(".");
+    if(textoVet[1].length == 1)
+        textoVet[1] += "0";  
+    textoVet[0] = formataMilhar(textoVet[0]);
+    return "R$ "+textoVet;
+}
+
+function formataMilhar(texto){
+    textoRev = texto.split("").reverse().join("");
+    var formatado = "";
+    for(i = 0; i < textoRev.length; i++){
+        if((i)%3 == 0 && i)
+            formatado += "."+textoRev[i];
+        else
+            formatado += textoRev[i];
+    }
+    return formatado.split("").reverse().join("");
 }
 
 calcula();
