f9a57d3ec7eac5324796b653f235cfef754bea34
Mayllon Veras <mayllonveras@gmail.com>
2018-11-20 21:36:53 -0300

    Aula do dia 20/11/2018

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function calcula(){
         var quantidade = $(produtos[i])
                         .find(".quantidade")[0].value;
         var subTotal = valor * quantidade;
-    $(produtos[i]).find(".subtotal").text(subTotal);
+    $(produtos[i]).find(".subtotal").text(realParaTexto(subTotal));
     total += subTotal;
     }
-    $("#total").text(total);
+    $("#total").text(realParaTexto(total));
 }
 
 function textoParaReal(texto) {
@@ -21,6 +21,37 @@ function textoParaReal(texto) {
     return parseFloat(textoLimpo);
 }
 
+function realParaTexto(valorReal){
+    var texto = ""+valorReal;
+    var textoVetor = [];
+    textoVetor = texto.split(".");
+    if(!textoVetor[1]){
+        textoVetor[1] = "00";
+    }else{
+        textoVetor[1] = textoVetor[1].substring(0, 2);
+    }
+     if(textoVetor[1].length == 1){
+         textoVetor[1] += "0";
+     }
+    var formatado = formataMilhar(textoVetor[0]);
+    var textoCompleto = "R$ "+formatado+","+textoVetor[1];
+    return textoCompleto;
+}
+
+
+function formataMilhar(valor){
+    var textoReverso = valor.split("").reverse().join("");
+    var textoFormatado = ""; 
+    for(var i = 0; i < textoReverso.length; i++){
+        if(i%3 == 0 && i != 0){
+            textoFormatado += "."+textoReverso[i];
+        }else{
+            textoFormatado += textoReverso[i];
+        }
+    }
+    return textoFormatado.split("").reverse().join("");
+    
+}
 
 calcula();
 $(".quantidade").change(function(){
